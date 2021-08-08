2cf28625006e114b32aeb5fef545f3470eb7a99e
Noor Octavian Anwar <alvin.ndsi@gmail.com>
2021-08-08 22:59:44 +0700

    Update the landing page layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,54 @@
-import { Button, Heading, Text, VStack } from '@chakra-ui/react';
+import { Box, Button, Center, Heading, Text, VStack } from '@chakra-ui/react';
 import Link from 'next/link';
 
 export default function Index() {
-	return <VStack
-		justifyContent={'center'}
-		p={6}
-		spacing={12}
-		minH={'90vh'}
-	>
-		<Heading
-			fontWeight={0}
-			textAlign={'center'}
+	return <Center h={'100vh'} flexDirection={'column'}>
+		<VStack
+			justifyContent={'center'}
+			p={6}
+			spacing={12}
+			minH={'90vh'}
 		>
-			My Projects Learning <br/>
-			Machine Learning<br/>
-			Give It Try<br/>
-			🚀
-
-		</Heading>
-		<Text textAlign={'center'}>
-			Cum elogium peregrinatione, omnes aususes carpseris fidelis, placidus decores.
-			Talis, alter hydras sensim quaestio de regius, superbus mortem.Elogiums experimentum!
-			Epos volares, tanquam noster quadra.Armariums ire in tornacum!Ubi est rusticus habitio?
-		</Text>
-		<VStack>
-			<Link passHref href={'/object-detection'}>
-				<Button
-					as={'a'}
-					colorScheme={'linkedin'}
-					rounded={'full'}
-				>
-					Object Detection 📹
-				</Button>
-			</Link>
-			<Link passHref href={'/handwriting-number-detection'}>
-				<Button
-					as={'a'}
-					colorScheme={'linkedin'}
-					rounded={'full'}
-				>
-					Handwriting Detection ✍
-				</Button>
-			</Link>
+			<Heading
+				fontWeight={0}
+				textAlign={'center'}
+				mt={'-6'}
+			>
+				My Projects Learning <br/>
+				Machine Learning<br/>
+				Give It Try<br/>
+				🚀
+			</Heading>
+			<VStack>
+				<Link passHref href={'/object-detection'}>
+					<Button
+						as={'a'}
+						colorScheme={'linkedin'}
+						rounded={'full'}
+						size={'sm'}
+					>
+						Object Detection 📹
+					</Button>
+				</Link>
+				<Link passHref href={'/handwriting-number-detection'}>
+					<Button
+						as={'a'}
+						colorScheme={'linkedin'}
+						rounded={'full'}
+						size={'sm'}
+					>
+						Handwriting Detection ✍
+					</Button>
+				</Link>
+			</VStack>
 		</VStack>
-	</VStack>
+		<Box h={3}/>
+		<Box>
+			<Text fontSize={'x-small'} textAlign={'center'}>
+				© 2021 Noor Octavian Anwar
+				<br/>
+				All right reserved.
+			</Text>
+		</Box>
+	</Center>
 }
